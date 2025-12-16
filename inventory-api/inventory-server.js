25e54c46b32443405491c997aa04b843744b0004
adamsdave <davidkenechukwu591@gmail.com>
2025-12-16 15:52:33 +0100

    Done

--- inventory-api/inventory-server.js
+++ inventory-api/inventory-server.js
@@ -4,7 +4,13 @@ const path = require("path")
 
 const dbPath = path.join(__dirname, "db", "items.json")
 
-const { createItem, getAllItems } = require("./request")
+const {
+   createItem,
+   getAllItems,
+   getEachItem,
+   updateItem,
+   deleteItem,
+} = require("./request")
 
 const PORT = 8000
 const HOSTNAME = "localhost"
@@ -12,18 +18,23 @@ const HOSTNAME = "localhost"
 function requestHandler(req, res) {
    res.setHeader("Content-Type", "text/html")
 
-   console.log(req.url)
-
    if (req.method == "POST" && req.url == "/items") {
       createItem(req, res)
    } else if (req.method == "GET" && req.url == "/items") {
       getAllItems(req, res)
-   } else if (req.method == "GET" && req.url == "/items") {
-      getOneItems(req, res)
+   } else if (req.method == "GET" && req.url.startsWith("/items/")) {
+      getEachItem(req, res)
    } else if (req.method == "PUT" && req.url == "/items") {
       updateItem(req, res)
    } else if (req.method == "DELETE" && req.url == "/items") {
       deleteItem(req, res)
+   } else {
+      res.writeHead(400)
+      res.end(
+         JSON.stringify({
+            message: "Route not found",
+         })
+      )
    }
 }
 
--- inventory-api/request.js
+++ inventory-api/request.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 const dbPath = path.join(__dirname, "db", "items.json")
 
-console.log(dbPath)
+// ADD ITEMS
 
 function createItem(req, res) {
    const body = []
@@ -21,7 +21,6 @@ function createItem(req, res) {
       console.log(parsedBody)
 
       fs.readFile(dbPath, "utf-8", (err, data) => {
-         console.log("I am here")
          if (err) {
             res.writeHead(400)
             res.end("Error in reading the file")
@@ -44,6 +43,7 @@ function createItem(req, res) {
    })
 }
 
+// GET ALL ITEMS
 function getAllItems(req, res) {
    fs.readFile(dbPath, "utf-8", (err, data) => {
       if (err) {
@@ -56,7 +56,133 @@ function getAllItems(req, res) {
    })
 }
 
+// GET SPECFIC ITEMS BY ID
+
+function getEachItem(req, res) {
+   //    const getId = req.url.split("/")
+   //    const id = Number(getId[2])
+
+   // OR
+   const id = Number(req.url.split("/")[2])
+
+   console.log(id)
+
+   fs.readFile(dbPath, "utf-8", (err, data) => {
+      if (err) {
+         res.writeHead(400)
+         res.end("Error in reading the file")
+      }
+
+      const allData = JSON.parse(data)
+      const item = allData.find((items) => items.id == id)
+
+      if (!item) {
+         res.writeHead(400)
+         res.end("Item not found!")
+
+         return
+      }
+
+      res.writeHead(200)
+      res.end(JSON.stringify(item))
+   })
+}
+
+// UPDATE SPECIFIC ITEM BY ID
+function updateItem(req, res) {
+   const body = []
+
+   req.on("data", (chunk) => {
+      body.push(chunk)
+   })
+
+   req.on("end", async () => {
+      const stringBody = Buffer.concat(body).toString()
+      const parsedBody = JSON.parse(stringBody)
+
+      fs.readFile(dbPath, "utf-8", (err, data) => {
+         if (err) {
+            res.writeHead(400)
+            res.end("Error in reading the file")
+         }
+
+         const oldData = JSON.parse(data)
+         const index = oldData.findIndex((items) => items.id == parsedBody.id)
+
+         if (index == -1) {
+            res.writeHead(400)
+            res.end("Item with the specified ID not found!")
+            return
+         }
+         const updatedData = { ...oldData[index], ...parsedBody }
+         oldData[index] = updatedData
+
+         fs.writeFile(dbPath, JSON.stringify(oldData), (err) => {
+            if (err) {
+               res.writeHead(400)
+               res.end("Error in updating file")
+            }
+
+            console.log("File updated successfully!")
+            res.writeHead(200)
+            res.end(JSON.stringify(oldData))
+         })
+      })
+   })
+}
+
+// DELETE ITEM BY ID
+
+function deleteItem(req, res) {
+   const body = []
+
+   req.on("data", (chunk) => {
+      body.push(chunk)
+   })
+
+   console.log(body)
+
+   req.on("end", async () => {
+      const stringBody = Buffer.concat(body).toString()
+      const parsedBody = JSON.parse(stringBody)
+
+      console.log(parsedBody)
+
+      fs.readFile(dbPath, "utf-8", (err, data) => {
+         if (err) {
+            res.writeHead(400)
+            res.end("Error in reading the file")
+            return
+         }
+
+         const oldData = JSON.parse(data)
+
+         if (oldData.length < parsedBody.id) {
+            res.writeHead(400)
+            res.end("Item not found")
+            return
+         }
+         const updatedData = oldData.filter(
+            (items) => items.id != parsedBody.id
+         )
+
+         fs.writeFile(dbPath, JSON.stringify(updatedData), (err) => {
+            if (err) {
+               res.writeHead(400)
+               res.end("Error in writing file")
+            }
+
+            console.log("File Deleted successfully!")
+            res.writeHead(200)
+            res.end(JSON.stringify(updatedData))
+         })
+      })
+   })
+}
 module.exports = {
    createItem,
    getAllItems,
+   getEachItem,
+   updateItem,
+   deleteItem,
 }
